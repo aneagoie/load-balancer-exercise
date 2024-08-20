236151852dc88cc6cbb07932080ec33829308bcf
Andrei Neagoie <aneagoie@Andreis-MacBook-Pro-2.local>
2024-08-20 13:24:11 -0400

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const os = require('os');
-const http = require('http'); 
+const http = require('http');  
 
 const server = http.createServer(function (request, response) {
   if (request.url === '/favicon.ico') {
